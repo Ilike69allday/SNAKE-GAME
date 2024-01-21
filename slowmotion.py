71c81e505533d0c8bb78b4ee78dc4989e225ed97
CHEWXINNING <ellencxn@gmail.com>
2024-01-21 14:38:16 +0800

    arrange code

--- slowmotion.py
+++ slowmotion.py
@@ -12,9 +12,9 @@ screen = pygame.display.set_mode((SW, SH))
 pygame.display.set_caption("Super Snake!")
 clock = pygame.time.Clock()
 score = 0
-snake_slowed_down = False
-snake_slow_down_start_time = 0
-eat_power_time = 0
+snakeslowdown = False
+slowdownstart = 0
+eatpower_time = 0
 spawn_time = 0
 
 class Snake:
@@ -27,7 +27,7 @@ class Snake:
         self.dead = False
 
     def update(self):
-        global apple, score, powerup, clock, snake_slowed_down, snake_slow_down_start_time, eat_power_time
+        global apple, score, powerup, clock, snakeslowdown, slowdownstart, eatpower_time
 
         for square in self.body:
             if self.head.x == square.x and self.head.y == square.y:
@@ -44,13 +44,15 @@ class Snake:
             self.dead = False
             score = 0
             apple = Apple()
-            clock.tick(8)
-            snake_slowed_down = False
-            eat_power_time = 0
+            clock.tick(8)  #initial speed of snake
+            snakeslowdown = False
+            eatpower_time = 0
 
         self.body.append(self.head)
+
         for i in range(len(self.body) - 1):
             self.body[i].x, self.body[i].y = self.body[i + 1].x, self.body[i + 1].y
+
         self.head.x += self.xdir * BLOCK_SIZE
         self.head.y += self.ydir * BLOCK_SIZE
         self.body.remove(self.head)
@@ -66,9 +68,9 @@ class Apple:
 
 class Powerup:
     def __init__(self):
-        self.spawn_new_powerup()
+        self.spawn_newpowerup()
 
-    def spawn_new_powerup(self):
+    def spawn_newpowerup(self):
         self.x = int(random.randint(0, SW - BLOCK_SIZE) / BLOCK_SIZE) * BLOCK_SIZE
         self.y = int(random.randint(0, SH - BLOCK_SIZE) / BLOCK_SIZE) * BLOCK_SIZE
         self.rect = pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE)
@@ -92,13 +94,16 @@ snake = Snake()
 apple = Apple()
 
 powerup_timer = pygame.USEREVENT + 1
-powerup_spawn_time = 30000  # spawn one in every 30 seconds
+
+powerup_spawn_time = 30000  # a new poer up will spawn in every 30 seconds
+
 pygame.time.set_timer(powerup_timer, powerup_spawn_time)
 
 powerup = None  # Initialize power-up object outside the loop
 
 snake_speed = 1
-snake_update_frequency = 8
+
+snake_update_frequency = 8 
 
 while True:
     for event in pygame.event.get():
@@ -120,13 +125,14 @@ while True:
                 snake.ydir = 0
         if event.type == powerup_timer:
             powerup = Powerup()
-            snake.snake_slowed_down = False
+            snake.snakeslowdown = False
             spawn_time = pygame.time.get_ticks()
 
 
     snake.update()
 
     screen.fill("black")
+
     drawGrid()
 
     apple.update()
@@ -138,7 +144,6 @@ while True:
 
     pygame.draw.rect(screen, "green", snake.head)
 
-
     for square in snake.body:
         pygame.draw.rect(screen, "green", square)
 
@@ -149,20 +154,20 @@ while True:
         apple = Apple()
         score += 1
 
-    if pygame.time.get_ticks() - eat_power_time > 10000:
-        snake.snake_slowed_down = False
+    if pygame.time.get_ticks() - eatpower_time > 10000:
+        snake.snakeslowdown = False
         snake_update_frequency = 8
     
-    if pygame.time.get_ticks() - spawn_time >10000:
+    if pygame.time.get_ticks() - spawn_time >10000: #The powerup only appear on screen for 10 second and it will disappeared
         powerup = None
 
     # Check for collision with power-up
     if powerup and snake.head.colliderect(powerup.rect):
         powerup = None
-        snake.snake_slowed_down = True
-        snake_update_frequency = 4
-        eat_power_time = pygame.time.get_ticks()
-        score += 3
+        snake.snakeslowdown = True
+        snake_update_frequency = 4  #slow down the speed of snake by changing clock.tick
+        eatpower_time = pygame.time.get_ticks()
+        score += 3 
 
     pygame.display.update()
     clock.tick(snake_update_frequency)
