7f01453e15b2c7a1da5283833f28aee070db45c3
CHEWXINNING <ellencxn@gmail.com>
2024-01-26 12:31:01 +0800

    add comment

--- obstaclesmod.py
+++ obstaclesmod.py
@@ -4,6 +4,7 @@ import random
 
 pygame.init()
 
+#set up the screen size, block size and font
 SW, SH = 750,750
 BLOCK_SIZE = 25 
 FONT = pygame.font.Font("font.ttf",BLOCK_SIZE*2)
@@ -11,7 +12,7 @@ FONT = pygame.font.Font("font.ttf",BLOCK_SIZE*2)
 screen = pygame.display.set_mode ((SW, SH))
 pygame.display.set_caption("Obstacles - Super Snake!")
 clock = pygame.time.Clock()
-score = 0
+score = 0 
 high_score=0
 
 try:
@@ -24,6 +25,7 @@ except (FileNotFoundError, ValueError):
 
 class Snake:
     def __init__(self):
+        #initialize the snake's body and position
         self.x ,self.y = BLOCK_SIZE, BLOCK_SIZE
         self.xdir = 1
         self.ydir = 0
@@ -34,6 +36,7 @@ class Snake:
     def update(self):
         global apple, score, obstacle, high_score
 
+        #check the collision with snake, screen border and obstacles
         for square in self.body:
             if self.head.x == square.x and self.head.y == square.y:
                 self.dead = True
@@ -43,9 +46,11 @@ class Snake:
                 if self.head.x == obs.x and self.head.y == obs.y:
                     self.dead = True
 
+        #update the high score
         if score > high_score:
             high_score = score
 
+        #restart the game when the snake is dead
         if self.dead:
             pygame.time.delay(2000)
             self.x, self.y = BLOCK_SIZE, BLOCK_SIZE
@@ -58,6 +63,7 @@ class Snake:
             apple = Apple()
             obstacle = [Obstacle()]
 
+        #increase the length of snake's body
         self.body.append(self.head)
         for i in range(len(self.body)-1):
             self.body[i].x, self.body[i].y = self.body[i+1].x , self.body[i+1].y
@@ -69,18 +75,18 @@ class Apple:
     def __init__(self):
         self.spawn_new_apple()
 
-    def spawn_new_apple(self):
+    def spawn_new_apple(self):  #generate a new apple
         self.x = int(random.randint(0, SW - BLOCK_SIZE)/BLOCK_SIZE) *BLOCK_SIZE
         self.y = int(random.randint(0, SH - BLOCK_SIZE)/BLOCK_SIZE) *BLOCK_SIZE
         self.rect = pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE)
 
-    def update(self):
+    def update(self):  #check the collision of apple and obstacles
         for obs in obstacle:
             if self.x == obs.x and self.y == obs.y:
                 self.spawn_new_apple()
         pygame.draw.rect(screen, "red", self.rect)
 
-        
+#representing the white obstacles on screen       
 class Obstacle:
     def __init__(self):
         self.x = int(random.randint(0, SW)/BLOCK_SIZE) *BLOCK_SIZE
@@ -90,12 +96,14 @@ class Obstacle:
     def update(self):
         pygame.draw.rect(screen, "white", self.rect)
 
+#function used to draw the grid on screen
 def drawGrid():
     for x in range (0,SW,BLOCK_SIZE):
         for y in range (0,SH,BLOCK_SIZE):
             rect = pygame.Rect(x,y,BLOCK_SIZE,BLOCK_SIZE)
             pygame.draw.rect(screen,"#3c3c3b",rect, 1)
 
+#score display
 score_text = FONT.render("1", True, "white")
 score_rect = score_text.get_rect(center=(SW/2.05, SH/20))
 
@@ -112,7 +120,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:  #change the direction of snake by using keyboard
             if event.key == pygame.K_DOWN:
                 snake.ydir = 1
                 snake.xdir = 0
@@ -147,12 +155,13 @@ while True:
     screen.blit(score_text, score_rect)
     screen.blit(high_score_text, (10, 10))
 
+    #check whether the snake eat the apple or not, score increased 1 if the apple eaten
     if snake.head.x == apple.x and snake.head.y == apple.y:
         snake.body.append(pygame.Rect(square.x, square.y, BLOCK_SIZE, BLOCK_SIZE))
         apple = Apple()
         score += 1 
 
-        obstacle.append(Obstacle())
+        obstacle.append(Obstacle())  #add a new obstacles everytime the snake eats an apple
 
     with open('hs_obstacles.txt', 'w') as file:
         file.write(str(high_score))
--- slowmotion.py
+++ slowmotion.py
@@ -4,6 +4,7 @@ import random
 
 pygame.init()
 
+#set up the screen size, block size and font
 SW, SH = 750, 750
 BLOCK_SIZE = 25
 FONT = pygame.font.Font("font.ttf", BLOCK_SIZE * 2)
@@ -11,6 +12,8 @@ FONT = pygame.font.Font("font.ttf", BLOCK_SIZE * 2)
 screen = pygame.display.set_mode((SW, SH))
 pygame.display.set_caption("Super Snake!")
 clock = pygame.time.Clock()
+
+#initialize the variables
 score = 0
 snakeslowdown = False
 slowdownstart = 0
@@ -19,13 +22,14 @@ spawn_time = 0
 
 class Snake:
     def __init__(self):
+        #initialize the snake's body and position
         self.x, self.y = BLOCK_SIZE, BLOCK_SIZE
         self.xdir = 1
         self.ydir = 0
         self.head = pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE)
         self.body = [pygame.Rect(self.x - BLOCK_SIZE, self.y, BLOCK_SIZE, BLOCK_SIZE)]
         self.dead = False
-        self.snakeslowdown = False
+        self.snakeslowdown = False  #check if the snake is under the slowdown power up
 
     def update(self):
         global apple, score, powerup, clock, snakeslowdown, slowdownstart, eat_power_time
@@ -36,6 +40,7 @@ class Snake:
             if self.head.x not in range(0, SW) or self.head.y not in range(0, SH):
                 self.dead = True
 
+        #restart the game when the snake is dead and reset the variable
         if self.dead:
             self.x, self.y = BLOCK_SIZE, BLOCK_SIZE
             self.head = pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE)
@@ -49,11 +54,10 @@ class Snake:
             snakeslowdown = False
             eat_power_time = 0
 
+        #increase the length of snake's body
         self.body.append(self.head)
-
         for i in range(len(self.body) - 1):
             self.body[i].x, self.body[i].y = self.body[i + 1].x, self.body[i + 1].y
-
         self.head.x += self.xdir * BLOCK_SIZE
         self.head.y += self.ydir * BLOCK_SIZE
         self.body.remove(self.head)
@@ -71,7 +75,7 @@ class Powerup:
     def __init__(self):
         self.spawn_newpowerup()
 
-    def spawn_newpowerup(self):
+    def spawn_newpowerup(self):  #generate a powerup at random position
         self.x = int(random.randint(0, SW - BLOCK_SIZE) / BLOCK_SIZE) * BLOCK_SIZE
         self.y = int(random.randint(0, SH - BLOCK_SIZE) / BLOCK_SIZE) * BLOCK_SIZE
         self.rect = pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE)
@@ -79,37 +83,37 @@ class Powerup:
     def draw(self):
         pygame.draw.rect(screen, "yellow", self.rect)
 
+#function used to draw the grid on screen
 def drawGrid():
     for x in range(0, SW, BLOCK_SIZE):
         for y in range(0, SH, BLOCK_SIZE):
             rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
             pygame.draw.rect(screen, "#3c3c3b", rect, 1)
 
+#score display
 score_text = FONT.render("1", True, "white")
 score_rect = score_text.get_rect(center=(SW / 2.05, SH / 20))
 
 drawGrid()
-
 snake = Snake()
-
 apple = Apple()
 
 powerup_timer = pygame.USEREVENT + 1
 
-powerup_spawn_time = 30000  # a new poer up will spawn in every 30 seconds
+powerup_spawn_time = 30000  # a new powerup spawn in every 30 seconds
 
 pygame.time.set_timer(powerup_timer, powerup_spawn_time)
 
-powerup = None  # Initialize power-up object outside the loop
+powerup = None  # Initialize powerup object outside the loop
 
-snake_update_frequency = 8 
+snake_update_frequency = 8  #initial snake update frequency
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:  #change the direction of snake by using keyboard
             if event.key == pygame.K_DOWN:
                 snake.ydir = 1
                 snake.xdir = 0
@@ -123,6 +127,7 @@ while True:
                 snake.xdir = -1
                 snake.ydir = 0
         if event.type == powerup_timer:
+
             powerup = Powerup()
             snake.snakeslowdown = False
             spawn_time = pygame.time.get_ticks()
@@ -133,7 +138,6 @@ while True:
     screen.fill("black")
 
     drawGrid()
-
     apple.update()
 
     if powerup:
@@ -148,23 +152,26 @@ while True:
 
     screen.blit(score_text, score_rect)
 
+    #check if the snake eat the apple or not
     if snake.head.x == apple.x and snake.head.y == apple.y:
         snake.body.append(pygame.Rect(square.x, square.y, BLOCK_SIZE, BLOCK_SIZE))
         apple = Apple()
         score += 1
 
+    #after 10 second the snake affected by the powerup, change back the snake's speed to initial update frequency
     if pygame.time.get_ticks() - eat_power_time > 10000:
         snake.snakeslowdown = False
         snake_update_frequency = 8
     
-    if pygame.time.get_ticks() - spawn_time >10000: #The powerup only appear on screen for 10 second and it will disappeared
+    #let the powerup only appear on screen for 10 second and it will be removed
+    if pygame.time.get_ticks() - spawn_time >10000: 
         powerup = None
 
     # Check for collision with power-up
     if powerup and snake.head.colliderect(powerup.rect):
         powerup = None
         snake.snakeslowdown = True
-        snake_update_frequency = 4  #slow down the speed of snake by changing clock.tick
+        snake_update_frequency = 4  #slow down the speed of snake by changing clock.tick from 8 to 4
         eat_power_time = pygame.time.get_ticks()
         score += 3 
 
