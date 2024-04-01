5ee7f6a5e839f9dc70571c1507b888da035488cf
JonathanK <145464014+Jonkwx27@users.noreply.github.com>
2024-04-01 18:22:04 +0800

    Update teleportationmod.py

--- teleportationmod.py
+++ teleportationmod.py
@@ -77,8 +77,8 @@ class Snake:
 
 class Apple:
     def __init__(self):
-        self.x = int(random.randint(0, SW - BLOCK_SIZE)/BLOCK_SIZE) *BLOCK_SIZE
-        self.y = int(random.randint(0, SH - BLOCK_SIZE)/BLOCK_SIZE) *BLOCK_SIZE
+        self.x = int(random.randint(0, SW)/BLOCK_SIZE) *BLOCK_SIZE
+        self.y = int(random.randint(0, SH)/BLOCK_SIZE) *BLOCK_SIZE
         self.rect = pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE)
 
     def update(self):
